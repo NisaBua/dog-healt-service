aa535c7ad76ccbedcf5f36b9f148ca13b8995b31
NisaBua <nisabualuang@gmail.com>
2022-07-29 23:21:20 +0700

    add detail

--- routes/api.js
+++ routes/api.js
@@ -8,60 +8,60 @@ const training_data = require('../models/training_data')
 
 module.exports = route
 
-route.get('/get_disease', dog_health_care.getDisease(), function (req, res) {
-  var response = res.data
-  res.status(200).json(response)
+route.get('/get_disease', dog_health_care.getDisease(), function(req, res) {
+    var response = res.data
+    res.status(200).json(response)
 })
 
-route.post('/login', user.login(), function (req, res) {
-  var response = res.data
-  res.status(200).json(response)
+route.post('/login', user.login(), function(req, res) {
+    var response = res.data
+    res.status(200).json(response)
 })
 
-route.post('/gen_password', user.genPassword(), function (req, res) {
-  var response = res.data
-  res.status(200).json(response)
+route.post('/gen_password', user.genPassword(), function(req, res) {
+    var response = res.data
+    res.status(200).json(response)
 })
 
 route.get(
-  '/get_profile',
-  auth.authentication(),
-  user.getProfile(),
-  function (req, res) {
-    var response = res.data
-    res.status(200).json(response)
-  }
+    '/get_profile',
+    auth.authentication(),
+    user.getProfile(),
+    function(req, res) {
+        var response = res.data
+        res.status(200).json(response)
+    }
 )
 
-route.get('/get_symptom', dog_health_care.getSymptom(), function (req, res) {
-  var response = res.data
-  res.status(200).json(response)
+route.get('/get_symptom', dog_health_care.getSymptom(), function(req, res) {
+    var response = res.data
+    res.status(200).json(response)
 })
 
-route.post('/update_symptom', dog_health_care.updateSymptom(), function (req, res) {
-  var response = res.data
-  res.status(200).json(response)
+route.post('/update_symptom', dog_health_care.updateSymptom(), function(req, res) {
+    var response = res.data
+    res.status(200).json(response)
 })
-route.post('/update_disease', dog_health_care.updateDisease(), function (req, res) {
-  var response = res.data
-  res.status(200).json(response)
+route.post('/update_disease', dog_health_care.updateDisease(), function(req, res) {
+    var response = res.data
+    res.status(200).json(response)
 })
 
 route.get(
-  '/get_symptom_of_disease',
-  dog_health_care.getSymptomOfDisease(),
-  function (req, res) {
-    var response = res.data
-    res.status(200).json(response)
-  }
+    '/get_symptom_of_disease',
+    dog_health_care.getSymptomOfDisease(),
+    function(req, res) {
+        var response = res.data
+        res.status(200).json(response)
+    }
 )
 route.post(
-  '/set_symptom_of_disease',
-  dog_health_care.setSymptomOfDisease(),
-  function (req, res) {
-    var response = res.data
-    res.status(200).json(response)
-  }
+    '/set_symptom_of_disease',
+    dog_health_care.setSymptomOfDisease(),
+    function(req, res) {
+        var response = res.data
+        res.status(200).json(response)
+    }
 )
 
 // route.get(
@@ -74,26 +74,26 @@ route.post(
 // )
 
 route.get(
-  '/get_first_node',
-  dog_health_care.getFirstNode(),
-  function (req, res) {
-    var response = res.data
-    res.status(200).json(response)
-  }
+    '/get_first_node',
+    dog_health_care.getFirstNode(),
+    function(req, res) {
+        var response = res.data
+        res.status(200).json(response)
+    }
 )
 
-route.post('/get_next_node', dog_health_care.getPredict(), function (req, res) {
-  var response = res.data
-  res.status(200).json(response)
+route.post('/get_next_node', dog_health_care.getNextNode(), function(req, res) {
+    var response = res.data
+    res.status(200).json(response)
 })
 
 route.post(
-  '/get_predict_disease',
-  dog_health_care.getPredictDisease(),
-  function (req, res) {
-    var response = res.data
-    res.status(200).json(response)
-  }
+    '/get_predict_disease',
+    dog_health_care.getPredictDisease(),
+    function(req, res) {
+        var response = res.data
+        res.status(200).json(response)
+    }
 )
 
 // route.post(
